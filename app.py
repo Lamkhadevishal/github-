ddf9a8df1b4fe1d0f18a359d324fa8fa66b4197d
lamkhadevishal <vishallamkhade21@gmail.com>
2026-06-22 17:33:16 +0530

    feat:modified app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-def main():
-    print("Hello from app.py")
-    print("This file is ready for your Python code.")
+def add():
+    print("This file is ready Hello")
+    print("This file is .pyfor your Python code ready Hello from app.pyfor your Python code.")
 
 
 if __name__ == "__main__":
-    main()
+    add()
